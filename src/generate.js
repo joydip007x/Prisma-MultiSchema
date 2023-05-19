a7b7c6bf34519d10775ad7926adfcadf1478dcee
joydip007x <joydip007x@gmail.com>
2023-05-19 19:25:18 +0600

    prisma generate ready

--- src/generate.js
+++ src/generate.js
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
-var { prismaUnifier } = require('prisma-unify007x');
-console.log('started Prisma-Unify');
-prismaUnifier();
+const { prismaUnifier } = require('prisma-unify007x');
+const { exec } = require('node:child_process');
+
+async function commands(){
+
+
+    console.log('started Prisma-Unify');
+    await prismaUnifier();
+    console.log('Wait ! Running npx prisma generate');
+    const gen= exec('npx prisma generate')
+    console.log('ProcessID- ',gen.pid,' Ended');
+    console.log('Run `npx prisma studio` to inspect your database');
+
+}
+commands();
